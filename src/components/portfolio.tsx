9d9f4c533cc2659c65677caf1fc2e80221e30cb9
Zoë <35260156+ZoeMesaros@users.noreply.github.com>
2023-02-17 21:52:59 +0100

    file updates

---
+++ src/components/portfolio.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { normalize } from 'path';
+import '../portfolio.scss';
+
+function Portfolio() {
+  return (
+    <div className="Portfolio">
+    <h1>Hej</h1>
+    </div>
+  );
+}
+
+export default Portfolio;
