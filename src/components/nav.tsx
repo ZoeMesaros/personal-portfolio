664bd9f57b8dac4822f8a782c21b54f38a81edae
Zoë <35260156+ZoeMesaros@users.noreply.github.com>
2023-02-19 19:46:23 +0100

    restructuring navlinks

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import { ReactDOM } from "react";
 
 const NavArray = [
-  "fa-solid fa-house",
-  "fa-solid fa-user",
-  "fa-solid fa-star",
-  "fa-solid fa-pen-to-square",
-  "fa-solid fa-envelope",
+  "fa-solid fa-house fa-xs",
+  "fa-solid fa-user fa-xs",
+  "fa-solid fa-star fa-xs",
+  "fa-solid fa-pen-to-square fa-xs",
+  "fa-solid fa-envelope fa-xs",
 ];
 
 export const NavBar = () => {
@@ -19,3 +19,37 @@ export const NavBar = () => {
 
   return <ul className="navUl">{NavList}</ul>;
 };
+
+
+const NavigationLinks = [
+  {
+    Name: "home",
+    iconName: "fa-solid fa-house fa-xs",,
+    Link: "https://docs.google.com/document/d/1NjCYVMQfduoGCQ_GD-19v7oJkVxnZqK18x-jojlRQjc/export?format=pdf",
+  },
+  {
+    Name: "LinkedIn",
+    iconName: "fa-brands fa-linkedin fa-stack-1x fa-inverse",
+    Link: "https://www.linkedin.com/in/zoe-km/",
+  },
+  {
+    Name: "GitHub",
+    iconName: "fa-brands fa-github  fa-stack-1x fa-inverse",
+    Link: "https://github.com/ZoeMesaros",
+  },
+];
+
+const GenerateLinks = () => (
+  <>
+    {SocialLinks.map((links, index) => (
+      <div className="linkIcon" key={index}>
+        <a href={links.Link} target="_blank">
+          <span className="fa-stack fa-lg">
+            <i className="fa fa-circle fa-stack-2x"></i>
+            <i className={links.iconName}></i>
+          </span>
+        </a>
+      </div>
+    ))}
+  </>
+);
