d346fbfa569fc12add3d3bc0bd97d7bb093b7960
Zoë <35260156+ZoeMesaros@users.noreply.github.com>
2023-02-21 13:49:29 +0100

    media-query-images-done

--- src/components/imgBg.tsx
+++ src/components/imgBg.tsx
@@ -5,15 +5,17 @@ import neonDev from "../assets/devBlue.png";
 export const BgImg = () => {
   return (
     <div className="imgContainer">
-      <figure className="helloText">
-        <img src={helloImg} alt="white text saying hello" />
-      </figure>
-      <figure className="neonText">
-        <img src={neonName} alt="Zoe keceli mesaros in pink neon letters" />
-      </figure>
-      <figure className="neonDev">
-        <img src={neonDev} alt="web developer in blue neon letters" />
-      </figure>
+      <div className="wrapper">
+        <figure className="helloText">
+          <img src={helloImg} alt="white text saying hello" />
+        </figure>
+        <figure className="neonText">
+          <img src={neonName} alt="Zoe keceli mesaros in pink neon letters" />
+        </figure>
+        <figure className="neonDev">
+          <img src={neonDev} alt="web developer in blue neon letters" />
+        </figure>
+      </div>
     </div>
   );
 };
