4e9ceec921369924605bfaecc0306935d26ca7c8
Zoë <35260156+ZoeMesaros@users.noreply.github.com>
2023-03-07 11:27:18 +0100

    skillspage

--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -4,16 +4,32 @@ import "./skills.scss";
 export const Skills = () => {
   return (
     <section id="skills">
-      <h1>Skills and tools used in school</h1>
+      <h1>Personal skills and tools</h1>
       <div className="container">
         <fieldset className="front">
           <legend>Frontend</legend>
+          <p className="icons">
+            <i className="fa-brands fa-html5"></i>
+            <i className="fa-brands fa-css3-alt"></i>
+            <i className="fa-brands fa-sass"></i>
+            <i className="fi fi-brands-js"></i>
+            <i className="fi fi-brands-typescript"></i>
+            <i className="fa-brands fa-react"></i>
+          </p>
         </fieldset>
         <fieldset className="back">
           <legend>Backend</legend>
+          <p className="icons">
+            MySQL Workbench<i className="fi fi-brands-mysql"></i>
+          </p>
         </fieldset>
         <fieldset className="tools">
           <legend>Tools</legend>
+          <p className="icons">
+            <i className="fa-brands fa-figma"></i>
+            <i className="fa-brands fa-git-alt"></i>
+            <i className="fi fi-brands-photoshop"></i>
+          </p>
         </fieldset>
       </div>
     </section>
