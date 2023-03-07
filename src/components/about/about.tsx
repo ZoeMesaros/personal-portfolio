81dd06c29830a203d5ba7844d93cd3d111ab1384
Zoë <35260156+ZoeMesaros@users.noreply.github.com>
2023-03-07 10:40:19 +0100

    skillspage

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -6,22 +6,22 @@ export const AboutMe = () => {
   return (
     <section id="about">
       <div className="textContainer">
-        <div className="wrapper">
-          <h1>Hello!</h1>
+        <fieldset>
+          <legend>
+            <h1>Hi There!</h1>
+          </legend>
           <p>
             My name is Zoë, I'm a developer student at Medieinstitutet in
-            Stockholm, Sweden. I am passionate about Web Development and I am
-            currently studying E-commerce within web development. Before this
-            school I had no previous experience with programming but I have
-            always loved computers. Immediately when I started this course I
-            knew I had ended up in the right place. My school is providing basic
-            knowledge in both back and frontend development such as HTML,
-            CSS/SCSS, Javascript, Typescript, API, Database, PHP and React. I am
-            eager to learn and challenge myself to grow my knowledge. I am
-            looking for LIA internship for autumn 2023 Between 2023-10-16 and
-            2023-12-24.
+            Stockholm, Sweden specializing in E-commerce. Before this school I
+            had no previous experience with programming but I have always loved
+            computers. Immediately when I started this course I knew I had ended
+            up in the right place. My school is providing basic knowledge in
+            both back and frontend development such as HTML, CSS/SCSS,
+            JavaScript, Typescript, API, Database, PHP and React. I am eager to
+            learn and challenge myself to grow my knowledge. I am looking for
+            LIA internship for autumn 2023 Between 2023-10-16 and 2023-12-24.
           </p>
-        </div>
+        </fieldset>
       </div>
     </section>
   );
--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -2,5 +2,20 @@ import React from "react";
 import "./skills.scss";
 
 export const Skills = () => {
-  return <section id="skills"></section>;
+  return (
+    <section id="skills">
+      <h1>Skills and tools used in school</h1>
+      <div className="container">
+        <fieldset className="front">
+          <legend>Frontend</legend>
+        </fieldset>
+        <fieldset className="back">
+          <legend>Backend</legend>
+        </fieldset>
+        <fieldset className="tools">
+          <legend>Tools</legend>
+        </fieldset>
+      </div>
+    </section>
+  );
 };
