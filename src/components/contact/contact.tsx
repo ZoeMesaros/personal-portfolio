fe21bad551e0d7bd7a19ac99a66d8d430caadd15
Zoë <35260156+ZoeMesaros@users.noreply.github.com>
2023-03-19 18:10:44 +0100

    contactform

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,8 +1,61 @@
 import React from "react";
+import { useState } from "react";
+import { send } from "emailjs-com";
 import "./contact.scss";
 
 export const Contact = () => {
-  return <section id="contact">
-    <h1>Contact me!</h1>
-  </section>;
+  const [toSend, setToSend] = useState({
+    from_name: "",
+    message: "",
+    reply_to: "",
+  });
+
+  const onSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    send("service_436coyf", "template_t1h9n1h", toSend, "6EjXbd1gFDjKfjaGs")
+      .then((response) => {
+        console.log("SUCCESS!", response.status, response.text);
+      })
+      .catch((err) => {
+        console.log("FAILED...", err);
+      });
+  };
+
+  const handleChange = (e: { target: { name: any; value: any } }) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
+  return (
+    <section id="contact">
+      <h1>Contact me!</h1>
+      <div className="container">
+        <form id="formContact" onSubmit={onSubmit}>
+          <label htmlFor="from_name">Name</label>
+          <input
+            type="text"
+            name="from_name"
+            value={toSend.from_name}
+            onChange={handleChange}
+          />
+          <label htmlFor="message">Message</label>
+          <input
+            type="text"
+            id="message"
+            name="message"
+            value={toSend.message}
+            onChange={handleChange}
+          />
+          <label htmlFor="reply_to">Email</label>
+          <input
+            type="text"
+            name="reply_to"
+            value={toSend.reply_to}
+            onChange={handleChange}
+          />
+          <button id="contact-submit" type="submit" data-submit="...Skickar">
+            Send
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
