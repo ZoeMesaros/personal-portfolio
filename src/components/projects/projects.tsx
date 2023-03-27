f627275e8b49df96423b938c77e11e58a1c6296d
Zoë <35260156+ZoeMesaros@users.noreply.github.com>
2023-03-27 13:13:48 +0200

    updates

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -5,10 +5,18 @@ import { getImageUrl } from "./utils";
 export const Projects = () => {
   const projectItems = projectData.map((project) => {
     return (
-      <a key={project.id} href={project.url} target="_blank">
-        <img src={getImageUrl(project.imgSrc)} />
-      </a>
+      <div className="projectCard">
+        <a key={project.id} href={project.url} target="_blank">
+          <img src={getImageUrl(project.imgSrc)} />
+          <p className="desc">{project.desc}</p>
+        </a>
+      </div>
     );
   });
-  return <section id="projects">{projectItems}</section>;
+  return (
+    <section id="projects">
+      <h1>My projects</h1>
+      <div className="wrapper">{projectItems}</div>
+    </section>
+  );
 };
